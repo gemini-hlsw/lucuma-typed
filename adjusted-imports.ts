c5dbb8cc9c73a69ef3430fd4b2bb53073e57d2a7
Raúl Piaggio <rpiaggio@gemini.edu>
2025-11-21 18:51:39 -0300

    update highcharts. prune libraries.

---
+++ adjusted-imports.ts
@@ -0,0 +1,24 @@
+/*!*
+ *
+ *  Copyright (c) Highsoft AS. All rights reserved.
+ *
+ *!*/
+import "./options/ad";
+import "./options/ao";
+import "./options/apo";
+import "./options/area";
+import "./options/arearange";
+import "./options/areaspline";
+import "./options/areasplinerange";
+import "./options/aroonoscillator";
+import "./options/atr";
+import "./options/bar";
+import "./options/bb";
+import "./options/item";
+import "./options/line";
+import "./options/pc";
+import "./options/pointandfigure";
+import "./options/scatter";
+import "./options/series";
+import "./options/spline";
+import * as globals from "./globals";
