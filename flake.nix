f0732b796e54c710184ee8ebd208486a9cb70275
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2026-08-10 14:11:09 -0400

    Update flake

--- flake.nix
+++ flake.nix
@@ -8,26 +8,20 @@
   outputs = { self, nixpkgs, flake-utils, typelevel-nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs-x86_64 = import nixpkgs {
-            system = "x86_64-darwin";
-        };
-        scala-cli-overlay = final: prev: {
-            scala-cli = pkgs-x86_64.scala-cli;
-        };
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ typelevel-nix.overlay scala-cli-overlay];
+          overlays = [ typelevel-nix.overlays.default ];
         };
       in
       {
-        devShell = pkgs.devshell.mkShell {
+        devShells.default = pkgs.devshell.mkShell {
           imports = [ typelevel-nix.typelevelShell ];
           packages = [
-            pkgs.nodePackages.vscode-langservers-extracted
+            pkgs.vscode-langservers-extracted
           ];
           typelevelShell = {
             nodejs.enable = true;
-            jdk.package = pkgs.jdk17;
+            jdk.package = pkgs.jdk25;
           };
         };
       }
